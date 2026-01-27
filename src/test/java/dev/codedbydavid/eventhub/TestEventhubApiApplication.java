8afadc3cec89e82cca3ab3dc4ccc753eec86256b
Deviitt11 <david.collados11@gmail.com>
2026-01-27 02:25:52 +0100

    test(it): add Testcontainers postgres and events controller integration tests

--- src/test/java/dev/codedbydavid/eventhub/TestEventhubApiApplication.java
+++ src/test/java/dev/codedbydavid/eventhub/TestEventhubApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestEventhubApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(EventhubApiApplication::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(EventHubApiApplication::main).with(TestcontainersConfiguration.class).run(args);
 	}
 
 }
